06ca828fb3094150ffc1005e3dfa260ba964126a
Snyder-F8 <fridahnzomo857@gmail.com>
2025-12-18 09:59:51 +0300

    Changes in person and dog file

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+
+        # validate name ONLY if provided
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+                self._name = None
+            else:
+                self._name = name
+        else:
+            self._name = None
+
+        # validate breed ONLY if provided
+        if breed is not None:
+            if breed not in APPROVED_BREEDS:
+                print("Breed must be in list of approved breeds.")
+                self._breed = None
+            else:
+                self._breed = breed
+        else:
+            self._breed = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def breed(self):
+        return self._breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+
+        # validate name ONLY if provided
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+                self._name = None
+            else:
+                self._name = name.title()
+        else:
+            self._name = None
+
+        # validate job ONLY if provided
+        if job is not None:
+            if job not in APPROVED_JOBS:
+                print("Job must be in list of approved jobs.")
+                self._job = None
+            else:
+                self._job = job
+        else:
+            self._job = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def job(self):
+        return self._job
